d8b22d04c0598c1fa433ff8aff146b17ca4b4914
Melon-Lo <dp98062@gmail.com>
2024-02-21 16:21:29 +0800

    feat/ manage routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import MainPage from './pages/MainPage';
-
 import './App.css';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+
+// import components
+import MainPage from './pages/MainPage';
+import LoginPage from './pages/LoginPage';
+
+// import contexts
+import RWDContextProvider from './contexts/RWD';
 
 function App() {
   return (
-    <div className="App">
-      <MainPage />
-    </div>
+    <BrowserRouter>
+      <RWDContextProvider>
+        <Routes>
+          <Route path="login" element={<LoginPage />} />
+          <Route path="/" element={<MainPage />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </RWDContextProvider>    
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,13 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-import RWDContextProvider from './contexts/RWD';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RWDContextProvider>
-      <App />
-    </RWDContextProvider>
+    <App />
   </React.StrictMode>
 );
